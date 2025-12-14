60aaa296d38fd4b4e98dc470c097893f7594b69c
rosita22-are <rr8378451@gmail.com>
2025-12-13 20:54:07 -0500

    first commit

---
+++ Programacion tradicional.py
@@ -0,0 +1,23 @@
+
+# PROGRAMA TRADICIONAL: PROMEDIO SEMANAL
+
+
+def ingresar_temperaturas():
+    temperaturas = []
+    print("Ingrese las temperaturas de la semana (7 días):\n")
+    for i in range(7):
+        temp = float(input(f"Día {i+1}: "))
+        temperaturas.append(temp)
+    return temperaturas
+
+
+def calcular_promedio(temps):
+    return sum(temps) / len(temps)
+
+def main():
+    temps = ingresar_temperaturas()
+    promedio = calcular_promedio(temps)
+    print(f"\nEl promedio semanal es: {promedio:.2f}°C")
+
+
+main()
